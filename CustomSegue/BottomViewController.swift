6f43d285bd5389ede26f9471df9605455cd56257
tiagomartinho <tiago.martinho@xpeppers.com>
2015-04-16 22:31:59 +0200

    add top and bottom custom segue from main view controller add top and bottom vc

---
+++ CustomSegue/BottomViewController.swift
@@ -0,0 +1,9 @@
+//
+//  BottomViewController.swift
+//  CustomSegue
+//
+//  Created by Martinho on 16/04/15.
+//  Copyright (c) 2015 Martinho. All rights reserved.
+//
+
+import Foundation
--- CustomSegue/MainViewController.swift
+++ CustomSegue/MainViewController.swift
@@ -14,16 +14,23 @@ class MainViewController: GeneralViewController
         super.viewDidLoad()
         addSwipeGestureRecognizer(UISwipeGestureRecognizerDirection.Right,action:"showLeftViewController")
         addSwipeGestureRecognizer(UISwipeGestureRecognizerDirection.Left,action:"showRightViewController")
+        addSwipeGestureRecognizer(UISwipeGestureRecognizerDirection.Down,action:"showTopViewController")
+        addSwipeGestureRecognizer(UISwipeGestureRecognizerDirection.Up,action:"showBottomViewController")
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if let customSegue = segue as? LinearSegue {
-            if let id = customSegue.identifier {
-                if id == "idRightSegue" {
+            if let identifier = customSegue.identifier {
+                switch identifier {
+                case "idRightSegue":
                     customSegue.direction = Direction.RightToLeft
-                }
-                if id == "idLeftSegue" {
+                case "idLeftSegue":
                     customSegue.direction = Direction.LeftToRight
+                case "idBottomSegue":
+                    customSegue.direction = Direction.BottomToTop
+                case "idTopSegue":
+                    customSegue.direction = Direction.TopToBottom
+                default: break
                 }
             }
         }
@@ -33,16 +40,24 @@ class MainViewController: GeneralViewController
         showLeftViewController()
     }
     
-    func showLeftViewController() {
-        self.performSegueWithIdentifier("idLeftSegue", sender: self)
-    }
-    
     @IBAction func showRightViewController(sender: UIBarButtonItem) {
         showRightViewController()
     }
     
+    func showLeftViewController() {
+        self.performSegueWithIdentifier("idLeftSegue", sender: self)
+    }
+    
     func showRightViewController() {
         self.performSegueWithIdentifier("idRightSegue", sender: self)
+    }
+    
+    func showBottomViewController() {
+        self.performSegueWithIdentifier("idBottomSegue", sender: self)
+    }
+    
+    func showTopViewController() {
+        self.performSegueWithIdentifier("idTopSegue", sender: self)
     }
 }
 
