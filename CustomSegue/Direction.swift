bd229fab1cb39da9831d7ec0660052b5ccfe4c6e
tiagomartinho <tiago.martinho@xpeppers.com>
2015-04-16 21:53:21 +0200

    add horizontal custom segue

---
+++ CustomSegue/Direction.swift
@@ -0,0 +1,14 @@
+//
+//  Direction.swift
+//  CustomSegue
+//
+//  Created by Martinho on 16/04/15.
+//  Copyright (c) 2015 Martinho. All rights reserved.
+//
+
+import Foundation
+
+enum Direction {
+    case LeftToRight
+    case RightToLeft
+}
